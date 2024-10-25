ade82955b7659b4cc39b341d576d6357d6753007
Jaksa Vlahovic <jaksa.vlahovic1@gmail.com>
2024-10-25 18:58:12 +0200

    add join functionality

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -5,12 +5,54 @@ import { auth } from "./auth";
 const generateCode = () => {
   const code = Array.from(
     { length: 6 },
-    () =>
-      "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 36)],
+    () => "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 36)]
   ).join("");
 
   return code;
 };
+
+export const join = mutation({
+  args: {
+    joinCode: v.string(),
+    workspaceId: v.id("workspaces"),
+  },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const workspace = await ctx.db.get(args.workspaceId);
+
+    if (!workspace) {
+      throw new Error("Workspace not found");
+    }
+
+    if (workspace.joinCode !== args.joinCode.toLowerCase()) {
+      throw new Error("Invalid join code");
+    }
+
+    const existingMember = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (!existingMember) {
+      throw new Error("Already a member of this workspace");
+    }
+
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId: workspace._id,
+      role: "member",
+    });
+
+    return workspace._id;
+  },
+});
 
 export const newJoinCode = mutation({
   args: {
@@ -26,7 +68,7 @@ export const newJoinCode = mutation({
     const member = await ctx.db
       .query("members")
       .withIndex("by_workspace_id_user_id", (q) =>
-        q.eq("workspaceId", args.workspaceId).eq("userId", userId),
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId)
       )
       .unique();
 
@@ -108,6 +150,31 @@ export const get = query({
   },
 });
 
+export const getInfoById = query({
+  args: { id: v.id("workspaces") },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      return null;
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
+
+    const workspace = await ctx.db.get(args.id);
+
+    return {
+      name: workspace?.name,
+      isMember: !!member,
+    };
+  },
+});
+
 export const getById = query({
   args: { id: v.id("workspaces") },
   handler: async (ctx, args) => {
@@ -120,7 +187,7 @@ export const getById = query({
     const member = await ctx.db
       .query("members")
       .withIndex("by_workspace_id_user_id", (q) =>
-        q.eq("workspaceId", args.id).eq("userId", userId),
+        q.eq("workspaceId", args.id).eq("userId", userId)
       )
       .unique();
 
@@ -147,7 +214,7 @@ export const update = mutation({
     const member = await ctx.db
       .query("members")
       .withIndex("by_workspace_id_user_id", (q) =>
-        q.eq("workspaceId", args.id).eq("userId", userId),
+        q.eq("workspaceId", args.id).eq("userId", userId)
       )
       .unique();
 
@@ -177,7 +244,7 @@ export const remove = mutation({
     const member = await ctx.db
       .query("members")
       .withIndex("by_workspace_id_user_id", (q) =>
-        q.eq("workspaceId", args.id).eq("userId", userId),
+        q.eq("workspaceId", args.id).eq("userId", userId)
       )
       .unique();
 
